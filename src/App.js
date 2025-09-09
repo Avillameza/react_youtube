2e68a42ad0605977567b7fcdf10421ea2e380d5f
avillameza <villameza.ana@gmail.com>
2025-09-08 22:17:49 -0700

    Updated dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import YoutubePlayer from './components/YoutubePlayer';
+import CocktailCard from "./components/CocktailCard";
+import Display from "./components/Display"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <YoutubePlayer />
-      </header>
-    </div>
-  );
+// function App() {
+class App extends React.Component {
+  state = {
+    cocktails: [],
+    favorites: [],
+  };
+
+  componentDidMount() {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin")
+      .then((response) => response.json())
+      .then((response) =>
+        this.setState({
+          cocktails: response.drinks,
+        })
+      );
+  }
+
+  showCocktails = () => {
+    return this.state.cocktails.map((cocktail) => (
+      <CocktailCard key={cocktail.id} addToFavorites ={this.addToFavorites} cocktail={cocktail} />
+    ));
+  };
+
+  showFavorites = () => {
+    return this.state.favorites.map((cocktail) => (
+      <CocktailCard key={cocktail.id} removeFromFavorites ={this.removeFromFavorites} cocktail={cocktail} />
+    ));
+  };
+
+  addToFavorites =(cocktail)=>{
+      if(!this.state.favorites.find(drink=>drink.idDrink === cocktail.idDrink))
+      this.setState({
+          favorites: [...this.state.favorites, cocktail]
+      })
+  }
+
+  removeFromFavorites =(cocktail)=>{
+    const filtered = this.state.favorites.filter(drink=>drink.idDrink !== cocktail.idDrink)
+    this.setState({
+        favorites: filtered
+    })
+}
+
+  render() {
+    return (
+     <Display showCocktails ={this.showCocktails} showFavorites = {this.showFavorites}/>
+    );
+  }
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <YoutubePlayer />
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
